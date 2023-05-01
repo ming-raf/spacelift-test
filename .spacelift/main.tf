3f3dbeef7068c51447ff7b34bc0517a78b91f4f4
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-02 01:09:26 +1000

    create worker pool

--- .spacelift/main.tf
+++ .spacelift/main.tf
@@ -4,10 +4,29 @@ terraform {
       source = "spacelift-io/spacelift"
       version = "1.1.5"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.48.0"
+    }
   }
 }
 
 provider "spacelift" {}
+
+module "my_workerpool" {
+  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v1.5.0"
+
+  configuration = <<-EOT
+    export SPACELIFT_TOKEN="${var.worker_pool_config}"
+    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
+  EOT
+
+  min_size          = 1
+  max_size          = 5
+  worker_pool_id    = var.worker_pool_id
+  security_groups   = var.worker_pool_security_groups
+  vpc_subnets       = var.worker_pool_subnets
+}
 
 resource "spacelift_space" "development" {
   name = "development"
