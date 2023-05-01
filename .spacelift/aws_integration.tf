b54562284a7b197f66157a4d3e719aefff24dc05
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-02 01:45:19 +1000

    handle integration with tf

---
+++ .spacelift/aws_integration.tf
@@ -0,0 +1,56 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  role_name = "Spacelift-Deploy"
+  role_arn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.role_name}"
+}
+
+resource "spacelift_stack" "this" {
+  name         = "AWS Integration"
+  repository   = "spacelift-test"
+  branch       = "main"
+}
+
+resource "spacelift_aws_integration" "this" {
+  name = local.role_name
+
+  # We need to set this manually rather than referencing the role to avoid a circular dependency
+  role_arn                       = local.role_arn
+  generate_credentials_in_worker = false
+}
+
+# The spacelift_aws_integration_attachment_external_id data source is used to help generate a trust policy for the integration
+data "spacelift_aws_integration_attachment_external_id" "this" {
+  integration_id = spacelift_aws_integration.this.id
+  stack_id       = spacelift_stack.this.id
+  read           = true
+  write          = true
+}
+
+resource "aws_iam_role" "this" {
+  name = local.role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      jsondecode(data.spacelift_aws_integration_attachment_external_id.this.assume_role_policy_statement),
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+  role       = aws_iam_role.this.name
+}
+
+resource "spacelift_aws_integration_attachment" "this" {
+  integration_id = spacelift_aws_integration.this.id
+  stack_id       = spacelift_stack.this.id
+  read           = true
+  write          = true
+
+  # The role needs to exist before we attach since we test role assumption during attachment.
+  depends_on = [
+    aws_iam_role.this
+  ]
+}
--- .spacelift/main.tf
+++ .spacelift/main.tf
@@ -17,21 +17,6 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-module "my_workerpool" {
-  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v1.5.0"
-
-  configuration = <<-EOT
-    export SPACELIFT_TOKEN="${var.worker_pool_config}"
-    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
-  EOT
-
-  min_size          = 1
-  max_size          = 5
-  worker_pool_id    = var.worker_pool_id
-  security_groups   = var.worker_pool_security_groups
-  vpc_subnets       = var.worker_pool_subnets
-}
-
 resource "spacelift_space" "development" {
   name = "development"
 
@@ -42,33 +27,4 @@ resource "spacelift_space" "development" {
 
   # An optional description of a space.
   description = "This a child of the root space. It contains all the resources common to the development infrastructure."
-}
-
-resource "spacelift_stack" "app-cdk" {
-
-  github_enterprise {
-    namespace = "ming-raf"
-  }
-
-  cloudformation {
-    entry_template_file = "cdk.out/CdkStack.template.json"
-    region              = "ap-southeast-2"
-    template_bucket     = "s3://spacelift-test"
-    stack_name          = "app-cdk"
-  }
-
-  administrative = true
-  autodeploy   = true
-  space_id     = "development"
-  branch       = "main"
-  description  = "Typical CDK stack"
-  name         = "Application CDK"
-  project_root = "cdk"
-  repository   = "spacelift-test"
-  runner_image = "public.ecr.aws/s5n0e7e5/ming-spacelift:latest"
-  before_plan  = [
-    "cdk bootstrap",
-    "cdk synth --output cdk/cdk.out",
-    "cdk ls",
-  ]
 }
