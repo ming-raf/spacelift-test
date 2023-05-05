f526ab440f5239f92de8248c6f6da401d83a6ded
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-06 02:36:44 +1000

    interim

---
+++ .spacelift/spaces/development/modules/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source = "spacelift-io/spacelift"
+      version = "1.1.5"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.65.0"
+    }
+  }
+}
+
+provider "spacelift" {}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
