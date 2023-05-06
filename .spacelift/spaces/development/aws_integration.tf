16bbe0326bf9717b9f46b3b0559393c2441d108a
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-06 22:22:03 +1000

    y

--- .spacelift/spaces/development/aws_integration.tf
+++ .spacelift/spaces/development/aws_integration.tf
@@ -20,8 +20,7 @@ resource "aws_iam_role" "this" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      jsondecode(data.spacelift_aws_integration_attachment_external_id.this.assume_role_policy_statement),
-      jsondecode(data.spacelift_aws_integration_attachment_external_id.ec2_worker_pool.assume_role_policy_statement),
+      jsondecode(data.spacelift_aws_integration_attachment_external_id.this.assume_role_policy_statement)
     ]
   })
 }
