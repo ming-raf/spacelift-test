b812a94bc6e565e01206b1e8babecbf41a197826
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-06 23:01:04 +1000

    create vpc

---
+++ .spacelift/spaces/development/vpc.tf
@@ -0,0 +1,118 @@
+# Create a VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16" # Replace with your desired CIDR block
+
+  tags = {
+    Name = "vpc"
+  }
+}
+
+# Create an internet gateway for the VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
+# Create a public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24" # Replace with your desired CIDR block
+  map_public_ip_on_launch = true # Enables auto-assigning of public IP addresses to instances
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Create a private subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24" # Replace with your desired CIDR block
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Create a route table for the VPC
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "route-table"
+  }
+}
+
+# Associate the public subnet with the route table
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Create a route for the internet gateway in the route table
+resource "aws_route" "internet_gateway_route" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0" # Route all traffic to the internet gateway
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Create a NAT gateway for the private subnet
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+# Create an Elastic IP for the NAT gateway
+resource "aws_eip" "eip" {
+  vpc = true
+
+  tags = {
+    Name = "eip"
+  }
+}
+
+# Create a route for the NAT gateway in the route table
+resource "aws_route" "nat_gateway_route" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0" # Route all traffic to the NAT gateway
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+}
+
+# Associate the private subnet with the route table
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_security_group" "open_sg" {
+  name_prefix = "sg-"
+  description = "Open all security group"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- .spacelift/spaces/development/worker_pool.tf
+++ .spacelift/spaces/development/worker_pool.tf
@@ -12,32 +12,18 @@ data "spacelift_environment_variable" "private_key" {
   depends_on = [ spacelift_context_attachment.worker_pool_worker_pool ]
 }
 
-# variable "SPACELIFT_POOL_PRIVATE_KEY" {
-#   type = string
+# module "my_workerpool" {
+#   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v1.5.0"
+
+#   configuration = <<-EOT
+#     export SPACELIFT_TOKEN="${spacelift_worker_pool.aws_ec2.config}"
+#     export SPACELIFT_POOL_PRIVATE_KEY="${data.spacelift_environment_variable.private_key.value}"
+#   EOT
+
+#   min_size          = 1
+#   max_size          = 5
+#   worker_pool_id    = spacelift_worker_pool.aws_ec2.id
+#   security_groups   = [ aws_security_group.open_sg.id ]
+#   vpc_subnets       = [ aws_subnet.private_subnet.id ]
+#   depends_on = [ data.spacelift_environment_variable.private_key ]
 # }
-
-variable "worker_pool_security_groups" {
-  type = list(string)
-  default = [ "sg-0ad7cbb72cb29255d" ]
-}
-
-variable "worker_pool_subnets" {
-  type = list(string)
-  default = [ "subnet-06337061", "subnet-7c864124", "subnet-88b0cdc1" ]
-}
-
-module "my_workerpool" {
-  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v1.5.0"
-
-  configuration = <<-EOT
-    export SPACELIFT_TOKEN="${spacelift_worker_pool.aws_ec2.config}"
-    export SPACELIFT_POOL_PRIVATE_KEY="${data.spacelift_environment_variable.private_key.value}"
-  EOT
-
-  min_size          = 1
-  max_size          = 5
-  worker_pool_id    = spacelift_worker_pool.aws_ec2.id
-  security_groups   = var.worker_pool_security_groups
-  vpc_subnets       = var.worker_pool_subnets
-  depends_on = [ data.spacelift_environment_variable.private_key ]
-}
