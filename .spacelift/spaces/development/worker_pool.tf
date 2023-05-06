7b54cc267d0dc38ed61a9c81eba7b575430fe69d
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-07 00:11:37 +1000

    Revert "dont need context lul"
    This reverts commit b5bed1d9424fc3f94a5e2e925364303e368a9b4c.

--- .spacelift/spaces/development/worker_pool.tf
+++ .spacelift/spaces/development/worker_pool.tf
@@ -6,12 +6,18 @@ resource "spacelift_worker_pool" "aws_ec2" {
   space_id    = data.spacelift_space_by_path.development.id
 }
 
+data "spacelift_environment_variable" "private_key" {
+  context_id = local.development_worker_pool_context_id
+  name       = "TF_VAR_SPACELIFT_POOL_PRIVATE_KEY"
+  depends_on = [ spacelift_context_attachment.worker_pool_worker_pool ]
+}
+
 module "my_workerpool" {
   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v1.5.0"
 
   configuration = <<-EOT
     export SPACELIFT_TOKEN="${spacelift_worker_pool.aws_ec2.config}"
-    export SPACELIFT_POOL_PRIVATE_KEY="${spacelift_worker_pool.aws_ec2.private_key}"
+    export SPACELIFT_POOL_PRIVATE_KEY="${data.spacelift_environment_variable.private_key.value}"
   EOT
 
   min_size          = 1
@@ -19,4 +25,5 @@ module "my_workerpool" {
   worker_pool_id    = spacelift_worker_pool.aws_ec2.id
   security_groups   = [ aws_security_group.open_sg.id ]
   vpc_subnets       = [ aws_subnet.private_subnet.id ]
+  depends_on = [ data.spacelift_environment_variable.private_key ]
 }
