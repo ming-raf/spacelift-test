d3f5c08f925cbc533377b223a590dc16ce518e46
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-06 14:29:46 +1000

    integration

--- .spacelift/spaces/development/aws_integration.tf
+++ .spacelift/spaces/development/aws_integration.tf
@@ -49,3 +49,15 @@ resource "spacelift_aws_integration_attachment" "this" {
     aws_iam_role.this
   ]
 }
+
+resource "spacelift_aws_integration_attachment" "this" {
+  integration_id = spacelift_aws_integration.this.id
+  stack_id       = spacelift_stack.ec2_worker_pool.id
+  read           = true
+  write          = true
+
+  # The role needs to exist before we attach since we test role assumption during attachment.
+  depends_on = [
+    aws_iam_role.this
+  ]
+}
