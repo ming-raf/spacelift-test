99b567634693b707bce2959994fe9b532dcd9ff3
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-06 23:44:14 +1000

    try

--- .spacelift/spaces/development/vpc.tf
+++ .spacelift/spaces/development/vpc.tf
@@ -101,16 +101,9 @@ resource "aws_security_group" "open_sg" {
   description = "Open all security group"
   vpc_id = aws_vpc.vpc.id
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
