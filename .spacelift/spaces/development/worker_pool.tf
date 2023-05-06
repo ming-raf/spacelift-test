b5bed1d9424fc3f94a5e2e925364303e368a9b4c
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-06 23:58:27 +1000

    dont need context lul

--- .spacelift/spaces/development/worker_pool.tf
+++ .spacelift/spaces/development/worker_pool.tf
@@ -6,18 +6,12 @@ resource "spacelift_worker_pool" "aws_ec2" {
   space_id    = data.spacelift_space_by_path.development.id
 }
 
-data "spacelift_environment_variable" "private_key" {
-  context_id = local.development_worker_pool_context_id
-  name       = "TF_VAR_SPACELIFT_POOL_PRIVATE_KEY"
-  depends_on = [ spacelift_context_attachment.worker_pool_worker_pool ]
-}
-
 module "my_workerpool" {
   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v1.5.0"
 
   configuration = <<-EOT
     export SPACELIFT_TOKEN="${spacelift_worker_pool.aws_ec2.config}"
-    export SPACELIFT_POOL_PRIVATE_KEY="${data.spacelift_environment_variable.private_key.value}"
+    export SPACELIFT_POOL_PRIVATE_KEY="${spacelift_worker_pool.aws_ec2.private_key}"
   EOT
 
   min_size          = 1
@@ -25,7 +19,6 @@ module "my_workerpool" {
   worker_pool_id    = spacelift_worker_pool.aws_ec2.id
   security_groups   = [ aws_security_group.open_sg.id ]
   vpc_subnets       = [ aws_subnet.private_subnet.id ]
-  depends_on = [ data.spacelift_environment_variable.private_key ]
 }
 
 output "key" {
