32578c335de1c6f9db4c7bb1cb3a46496715280b
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-07 00:03:33 +1000

    a

--- .spacelift/spaces/development/worker_pool.tf
+++ .spacelift/spaces/development/worker_pool.tf
@@ -1,22 +1,23 @@
 
-resource "spacelift_worker_pool" "aws_ec2" {
-  name        = "AWS EC2 Worker Pool (Development)"
-  csr         = filebase64("../../spacelift.csr")
-  description = "Used for all type jobs"
-  space_id    = data.spacelift_space_by_path.development.id
-}
+# resource "spacelift_worker_pool" "aws_ec2" {
+#   name        = "AWS EC2 Worker Pool (Development)"
+#   csr         = filebase64("../../spacelift.csr")
+#   description = "Used for all type jobs"
+#   space_id    = data.spacelift_space_by_path.development.id
+# }
 
-module "my_workerpool" {
-  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v1.5.0"
+# module "my_workerpool" {
+#   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v1.5.0"
 
-  configuration = <<-EOT
-    export SPACELIFT_TOKEN="${spacelift_worker_pool.aws_ec2.config}"
-    export SPACELIFT_POOL_PRIVATE_KEY="${spacelift_worker_pool.aws_ec2.private_key}"
-  EOT
+#   configuration = <<-EOT
+#     export SPACELIFT_TOKEN="${spacelift_worker_pool.aws_ec2.config}"
+#     export SPACELIFT_POOL_PRIVATE_KEY="${spacelift_worker_pool.aws_ec2.private_key}"
+#   EOT
 
-  min_size          = 1
-  max_size          = 5
-  worker_pool_id    = spacelift_worker_pool.aws_ec2.id
-  security_groups   = [ aws_security_group.open_sg.id ]
-  vpc_subnets       = [ aws_subnet.private_subnet.id ]
-}
+#   min_size          = 1
+#   max_size          = 5
+#   worker_pool_id    = spacelift_worker_pool.aws_ec2.id
+#   security_groups   = [ aws_security_group.open_sg.id ]
+#   vpc_subnets       = [ aws_subnet.private_subnet.id ]
+#   depends_on = [ spacelift_worker_pool.aws_ec2 ]
+# }
