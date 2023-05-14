1270b9aba382127cd57f0ef60d7a5efd1a8e1f7f
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-14 14:20:23 +1000

    capability

--- .spacelift/spaces/development/contexts.tf
+++ .spacelift/spaces/development/contexts.tf
@@ -11,3 +11,20 @@ resource "spacelift_context_attachment" "worker_pool_worker_pool" {
   stack_id   = var.spacelift_stack_id
   priority   = 0
 }
+
+resource "spacelift_context" "lambda_cf" {
+  name        = "Lambda CloudFormation"
+}
+
+resource "spacelift_context_attachment" "lambda_cf" {
+  context_id = spacelift_context.lambda_cf.id
+  stack_id = spacelift_stack.lambda_cf.id
+  priority   = 0
+}
+
+resource "spacelift_environment_variable" "capability_iam" {
+  context_id   = spacelift_context.lambda_cf.id
+  name       = "CF_CAPABILITY_IAM"
+  value      = "1"
+  write_only = false
+}
