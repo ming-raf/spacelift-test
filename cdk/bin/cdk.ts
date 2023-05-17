c051cdf833ab19e6c3fcc97bbb9ef505758488c4
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-17 19:34:31 +1000

    try

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,4 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new CdkStack(app, 'CdkStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: "ap-southeast-2"
+    }
+});
