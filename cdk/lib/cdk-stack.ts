69041f09d62e5fb6bfa88ddbf18a0435a9a84b06
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-17 16:57:15 +1000

    remove everything

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -7,13 +7,5 @@ import { Construct } from 'constructs';
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
