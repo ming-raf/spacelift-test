6db0c11bf66ef8f9add2a20bc7150eff3b39a66b
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-05-17 17:34:04 +1000

    do ec2 instead

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,11 +1,20 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(this, 'Vpc', {
+      vpcId: "vpc-04067621ba51a40e8"
+    });
+
+    const instance = new ec2.Instance(this, 'Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+    });
+
   }
 }
