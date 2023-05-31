7674c32c4b07c57eead41e1f8292d6f2a2be0c86
rafiqicr <rafiqi.rosli@cashrewards.com>
2023-06-01 09:21:54 +1000

    Update main.tf

--- spacelift_modules/main.tf
+++ spacelift_modules/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    spacelift = {
-      source = "spacelift-io/spacelift"
-      version = "1.1.5"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.65.0"
-    }
-  }
-}
-
-provider "spacelift" {}
-
-provider "aws" {
-  region = "ap-southeast-2"
-}
-
 data "spacelift_stack" "dev_admin_stack" {
   stack_id = var.dev_admin_stack_name
 }
